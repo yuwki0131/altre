fdc1a2cdc78add9eec1e53cdbd2a30d3fe935f7b
yuwki0131 <yuwkixxx@gmail.com>
2025-10-20 00:39:35 +0900

    remove unused files

--- build.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    if std::env::var_os("CARGO_FEATURE_GUI").is_some() {
-        slint_build::compile("src/frontend/gui/components/main.slint")
-            .expect("Slint GUI コンポーネントの生成に失敗しました");
-    }
-}
